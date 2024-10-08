8e7758db803df3a42b246da044237cf9df7a313a
mayank <mayanksingh90200>
2024-10-08 21:41:38 +0530

    JS work

---
+++ import.mjs
@@ -0,0 +1,12 @@
+import { add, subtract, PI} from "./msd.mjs";
+
+import * as jadoo from "./msd.mjs";
+
+import multiply from "./msd.mjs";
+
+export {add,subtract,PI,multiply};
+console.log("add: ",jadoo.add(5,3));
+console.log("subtract: ",jadoo.subtract(5,3));
+console.log("PI: ",jadoo.PI);
+console.log("multiply: ",multiply(5,3));
+
